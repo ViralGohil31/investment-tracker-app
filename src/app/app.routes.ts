233edbe3d74a73add92b570d32456de5e2b99ebe
ViralGohil31 <viralgohil31@gmail.com>
2025-08-24 23:42:45 +0530

    Added Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InvestmentComponent } from './investment/investment.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "dashboard", component: DashboardComponent},
+    {path: "investment", component: InvestmentComponent}
+];
